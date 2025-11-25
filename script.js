6807d7a5c64bdc54393b252ab4cde1203078c81e
Venkatesh-5434 <venkateshkumaresan97@gmail.com>
2025-11-25 16:43:31 +0530

    Refactor alertMsg function and add script loader

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// Button click function
 function alertMsg() {
     alert("Welcome! Website is working.");
 }
+
+// ASYNCHRONOUS Zoho Pagesense Script Loader
+(function(w, s) {
+    var e = document.createElement("script");
+    e.type = "text/javascript";
+    e.async = true;
+    e.src = "https://cdn-in.pagesense.io/js/60057344075/837a5bc35feb43f59441d6fb4f3f38d1.js";
+    var x = document.getElementsByTagName("script")[0];
+    x.parentNode.insertBefore(e, x);
+})(window, "script");
